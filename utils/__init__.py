11e2a985f53cd5fb626a81430f741a3cc9719f09
fafu <fa.fu@casivision.com>
2023-12-14 14:37:16 +0800

    add collect_stats, compute_amax

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .my_logging import init_logging
 from .dataset import segDataset
 from .model import unet
 from .criterion import bootstrapped_cross_entropy2d, DiceLoss
+from .utils import collect_stats, compute_amax
