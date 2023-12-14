0f1e0d4f687785a38048cdee800fe044e8c0a71d
fafu <fa.fu@casivision.com>
2023-12-14 17:34:11 +0800

    添加矫正

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .dataset import segDataset
 from .model import unet, wrapUnet
 from .criterion import bootstrapped_cross_entropy2d, DiceLoss
 from .utils import collect_stats, compute_amax
+from .calibrator import Calibrator
---
+++ utils/calibrator.py
@@ -0,0 +1,76 @@
+import os
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+import logging
+import numpy as np
+import cv2
+
+class Calibrator(trt.IInt8EntropyCalibrator2):
+    def __init__(self, quantification=1, batch_size=1, height=256, width=256, calibration_images="", cache_file=""):
+        trt.IInt8EntropyCalibrator2.__init__(self)
+        self.index = 0
+        self.length = quantification
+        print("self.length:",self.length)
+        self.batch_size = batch_size
+        self.cache_file = cache_file
+        print("cache_file:",self.cache_file)
+        self.height = height
+        self.width = width
+        self.img_list = [calibration_images + '/' + l for l in os.listdir(calibration_images)]
+        #print("image list:",self.img_list)
+        self.length = len(self.img_list)
+        print("self.length:",self.length)
+        self.calibration_data = np.zeros((self.batch_size, 1, self.height, self.width), dtype=np.float32)
+        self.d_input = cuda.mem_alloc(self.calibration_data.nbytes)
+
+    def next_batch(self):
+        if self.index % 10 == 0:
+            print("Calibrating batch {:}, containing {:} images".format(self.index, self.batch_size))
+        if self.index < self.length:
+            for i in range(self.batch_size):
+                img = cv2.imread(self.img_list[i + self.index*self.batch_size],0)
+                img = np.expand_dims(img, axis=0).astype(np.float32)
+                self.calibration_data[i] = img
+            self.index += 1
+            return np.ascontiguousarray(self.calibration_data, dtype=np.float32)
+        else:
+            return np.array([])
+
+    def __len__(self):
+        return self.length
+
+    def get_batch_size(self):
+        return self.batch_size
+
+    def get_batch(self, name):
+        batch = self.next_batch()
+        if not batch.size:
+            return None
+        cuda.memcpy_htod(self.d_input, batch)
+        return [int(self.d_input)]
+
+    def read_calibration_cache(self):
+        # If there is a cache, use it instead of calibrating again. Otherwise, implicitly return None.
+        if os.path.exists(self.cache_file):
+            print("load cache {}.........".format(self.cache_file))
+            with open(self.cache_file, "rb") as f:
+                return f.read()
+
+    def write_calibration_cache(self, cache):
+        print("write cache {}.........".format(self.cache_file))
+        with open(self.cache_file, "wb") as f:
+            f.write(cache)
+
+    def preprocess(self, img):
+        print("Please check the preprocess function.")
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (self.width, self.height))
+        image = image.astype(np.float32)
+        image /= 255.0
+        mean = [0.4914, 0.4822, 0.4465]
+        std = [0.2023,0.1994, 0.2010]
+        image = (image - mean) / std
+        image = np.transpose(image, [2, 0, 1])
+        return image
+
