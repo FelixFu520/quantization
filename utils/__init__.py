87e683f3227e7f23f7a94db460f1a75b16adf0bc
fafu <fa.fu@casivision.com>
2023-12-14 16:54:11 +0800

    wrapUnet

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .my_logging import init_logging
 from .dataset import segDataset
-from .model import unet
+from .model import unet, wrapUnet
 from .criterion import bootstrapped_cross_entropy2d, DiceLoss
 from .utils import collect_stats, compute_amax
